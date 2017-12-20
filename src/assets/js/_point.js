0519a29632c68f577cd8aa5830ed73f6be9386e9
chiba <masa@masa.local>
2017-12-20 22:02:18 +0900

    とりあえず形になりました。
    少しずつキレイにします。頑張ります

--- src/assets/js/_point.js
+++
@@ -1,8 +0,0 @@
-
-const g = {
-  isSp:function(){ return ( g.winW() <= g.point )? true : false ; },// SP or PC
-  winW:function(){ return window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth; },
-  winH:function(){ return $( window ).height(); },
-  winT:function(){ return $( window ).scrollTop(); },
-  point: 768
-}
